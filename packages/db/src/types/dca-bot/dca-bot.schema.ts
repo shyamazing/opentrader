b99ffab702058f8f1f3a4038be523c96d9d801d6
bludnic <victorcpla@gmail.com>
2024-10-24 22:28:25 +0100

    feat(dca): define bot schema

--- packages/db/src/types/dca-bot/dca-bot.schema.ts
+++ packages/db/src/types/dca-bot/dca-bot.schema.ts
@@ -1,9 +1,27 @@
 import { z } from "zod";
-
 import { zt } from "@opentrader/prisma";
+
+import { XOrderType } from "@opentrader/types";
 import { ZBotState } from "../bot/bot-state.schema.js";
 
-export const ZDcaBotSettings = z.any(); // @todo use `dca.schema` from @opentrader/bot-templates
+// Keep `ZDcaBotSettings` in sync with `dca.schema` from @opentrader/bot-templates
+export const ZDcaBotSettings = z.object({
+  entry: z.object({
+    quantity: z.number().positive().describe("Quantity of the Entry Order in base currency"),
+    type: z.nativeEnum(XOrderType).describe("Entry with Limit or Market order"),
+    price: z.number().optional(),
+    conditions: z.any().optional(),
+  }),
+  tp: z.object({
+    percent: z.number().positive().describe("Take Profit from entry order price in %"),
+  }),
+  safetyOrders: z.array(
+    z.object({
+      quantity: z.number().positive().positive("Quantity of the Safety Order in base currency"),
+      priceDeviation: z.number().positive().positive("Price deviation from the Entry Order price in %"),
+    }),
+  ),
+});
 
 export const ZDcaBot = zt.BotSchema.extend({
   settings: ZDcaBotSettings,
