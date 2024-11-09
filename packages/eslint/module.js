e12bd51e66022582bede14846e85e1051a96c17a
bludnic <victorcpla@gmail.com>
2024-11-09 19:29:12 +0000

    chore(ESLint): disable "no-explicit-any" due false positive

--- packages/eslint/module.js
+++ packages/eslint/module.js
@@ -1,24 +1,25 @@
-import eslint from '@eslint/js'
-import tseslint from 'typescript-eslint'
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
 
 export default tseslint.config({
-  files: ['src/**/*.ts'],
+  files: ["src/**/*.ts"],
   plugins: {
-    '@typescript-eslint': tseslint.plugin
+    "@typescript-eslint": tseslint.plugin,
   },
   languageOptions: {
     parser: tseslint.parser,
     parserOptions: {
       project: true,
-      sourceType: 'module'
-    }
+      sourceType: "module",
+    },
   },
   extends: [eslint.configs.recommended, ...tseslint.configs.recommended],
   rules: {
-    '@typescript-eslint/no-unsafe-argument': 'error',
-    '@typescript-eslint/no-unsafe-assignment': 'off',
-    '@typescript-eslint/no-unsafe-call': 'warn',
-    '@typescript-eslint/no-unsafe-member-access': 'warn',
-    '@typescript-eslint/no-unsafe-return': 'error'
-  }
-})
+    "@typescript-eslint/no-unsafe-argument": "off",
+    "@typescript-eslint/no-unsafe-assignment": "off",
+    "@typescript-eslint/no-unsafe-call": "off",
+    "@typescript-eslint/no-unsafe-member-access": "off",
+    "@typescript-eslint/no-unsafe-return": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+  },
+});
