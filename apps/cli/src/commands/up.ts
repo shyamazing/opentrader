4f819835a2a63eead975bfab8c94db60a1dc677e
Michael Montanaro <mcmontanaro01@gmail.com>
2025-01-06 18:10:08 -0500

    updating addUpCommand to handle port flag

--- apps/cli/src/commands/up.ts
+++ apps/cli/src/commands/up.ts
@@ -3,5 +3,8 @@ import { handle } from "../utils/command.js";
 import { up } from "../api/up/index.js";
 
 export function addUpCommand(program: Command) {
-  program.command("up").addOption(new Option("-d, --detach", "Run in detached mode").default(false)).action(handle(up));
+  program.command("up")
+  .addOption(new Option("-d, --detach", "Run in detached mode").default(false)).action(handle(up))
+  .addOption(new Option("-p, --port <port>", "Customize port server should attach to").default(8000))
+  .action(handle(up));
 }
