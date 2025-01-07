6bbbb4fe5040e3b527c6c4b58ea4cc2c563080ed
Michael Montanaro <mcmontanaro01@gmail.com>
2025-01-07 18:24:19 -0500

    added help command

--- apps/cli/src/commands/up.ts
+++ apps/cli/src/commands/up.ts
@@ -4,7 +4,8 @@ import { up } from "../api/up/index.js";
 
 export function addUpCommand(program: Command) {
   program.command("up")
-  .addOption(new Option("-d, --detach", "Run in detached mode").default(false)).action(handle(up))
-  .addOption(new Option("-p, --port <port>", "Customize port server should attach to").default(8000))
-  .action(handle(up));
+    .addOption(new Option("-d, --detach", "Run in detached mode").default(false))
+    .addOption(new Option("-p, --port <port>", "Customize port server should attach to").default(8000))
+    .helpOption("-h, --help", "Display help for command")
+    .action(handle(up));
 }
