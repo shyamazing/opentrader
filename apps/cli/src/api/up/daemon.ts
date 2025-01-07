fbb9493a72989b0521462ed11455bb2bcb3d2547
Michael Montanaro <mcmontanaro01@gmail.com>
2025-01-07 18:14:42 -0500

    passing port to daemon

--- apps/cli/src/api/up/daemon.ts
+++ apps/cli/src/api/up/daemon.ts
@@ -1,9 +1,11 @@
 import { Daemon } from "@opentrader/daemon";
 
+const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 8000;
+
 const daemon = await Daemon.create({
   server: {
     frontendDistPath: "../frontend",
-    port: 8000,
+    port: port,
   },
 });
 
