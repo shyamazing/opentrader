eee4294131cf44eed3d525cfbbcd199d634071d4
Michael Montanaro <mcmontanaro01@gmail.com>
2025-01-07 19:55:30 -0500

    updating listen function to use custom domain rather than listen on all interfaces

--- packages/daemon/src/server.ts
+++ packages/daemon/src/server.ts
@@ -50,7 +50,7 @@ export const createServer = (params: CreateServerOptions) => {
     app: fastify,
     server: fastify.server,
     listen: async () => {
-      await fastify.listen({ port: params.port, host: params.domain }); // Listen on all interfaces, remove host to listen only on localhost
+      await fastify.listen({ port: params.port, host: params.domain }); // Change to 0.0.0.0 to always listen on all interfaces
     },
     close: async () => {
       await fastify.close();
