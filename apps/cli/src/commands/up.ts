8809a9297c93300aa120125de8159f92d74e553a
Michael Montanaro <mcmontanaro01@gmail.com>
2025-01-16 10:25:27 -0500

    fetched default options with new getSettings(), write any overwritten values back to settings.json

--- apps/cli/src/commands/up.ts
+++ apps/cli/src/commands/up.ts
@@ -1,12 +1,27 @@
 import { Command, Option } from "commander";
 import { handle } from "../utils/command.js";
+import { settingsPath } from "../utils/app-path.js";
 import { up } from "../api/up/index.js";
+import { writeFileSync } from "fs";
+
+import { getSettings } from "../utils/settings.js";
+
 
 export function addUpCommand(program: Command) {
+  const settings = getSettings();
+
   program.command("up")
-    .addOption(new Option("-d, --detach", "Run in detached mode").default(false))
-    .addOption(new Option("-p, --port <port>", "Customize port server should attach to").default(8000))
-    .addOption(new Option("-D, --domain <domain>", "Customize domain server should attach to").default("localhost"))
+    .addOption(new Option("-d, --detach", "Run in detached mode").default(settings.detach))
+    .addOption(new Option("-p, --port <port>", "Customize port server should attach to").default(settings.port))
+    .addOption(new Option("-D, --domain <domain>", "Customize domain server should attach to").default(settings.domain))
     .helpOption("-h, --help", "Display help for command")
-    .action(handle(up));
+    .action((options) => {
+      const newSettings = {
+        detach: options.detach,
+        port: options.port,
+        domain: options.domain
+      };
+      writeFileSync(settingsPath, JSON.stringify(newSettings, null, 2));
+      handle(up)(options);
+    });
 }
