0faba757d6df4709953f9403db4ce1fdb9a8f44e
bludnic <victorcpla@gmail.com>
2025-01-18 22:19:19 +0000

    refactor(daemon, up): remove unused imports

--- apps/cli/src/commands/up.ts
+++ apps/cli/src/commands/up.ts
@@ -1,16 +1,14 @@
 import { Command, Option } from "commander";
 import { handle } from "../utils/command.js";
-import { settingsPath } from "../utils/app-path.js";
 import { up } from "../api/up/index.js";
-import { writeFileSync } from "fs";
 
 import { getSettings } from "../utils/settings.js";
-
 
 export function addUpCommand(program: Command) {
   const settings = getSettings();
 
-  program.command("up")
+  program
+    .command("up")
     .addOption(new Option("-d, --detach", "Run in detached mode").default(false))
     .addOption(new Option("-p, --port <port>", "Customize port server should attach to").default(settings.port))
     .addOption(new Option("--host <host>", "Customize domain server should attach to").default(settings.host))
